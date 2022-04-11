85d360691a1437c5489ca58ce597601c5323e4b6
xxmafiaxxx <45888046+xxmafiaxxx@users.noreply.github.com>
2022-04-11 18:30:33 -0400

    test

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -8,7 +8,7 @@ import JupiterForm from "../JupiterForm/JupiterForm";
 
 import styles from "./index.module.css";
 
-const Home: FC = ({ }) => {
+const Home: FC = () => {
   return (
     <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
       <div className={styles.container}>
